a5e7491f2ec6cea3bfdfab6059b47298510e16d4
Rishij4 <162426055+Rishij4@users.noreply.github.com>
2026-07-04 20:40:00 +0530

    Change API base URL to production server

--- src/services/uploadService.js
+++ src/services/uploadService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ats-resume-analyzer-backend-enli.onrender.com/api",
 });
 
 export const uploadResume = async (formData) => {
