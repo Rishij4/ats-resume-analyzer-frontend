2cd1ea89dad768661007506d6b2af227f088e7a3
Rishij4 <162426055+Rishij4@users.noreply.github.com>
2026-07-04 20:39:20 +0530

    Update API base URL to production endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ats-resume-analyzer-backend-enli.onrender.com/api",
 });
 
 export default API;
